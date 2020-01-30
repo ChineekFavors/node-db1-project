5015db39253e90641136c514201e58e9fef3f46c
Chineek Favors <chineekfavors01@gmail.com>
2020-01-29 20:13:29 -0800

    created accountsRouter.js

---
+++ data/seeds/accountsRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const db = require('../dbConfig.js');
+
+const router = express.Router();
+
+router.get('/', (req,res) => {
+
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
 const db = require('./data/dbConfig.js');
+const accountsRouter = require('./data/seeds/accountsRouter.js');
 
 const server = express();
 
 server.use(express.json());
+
+ server.use('/accounts', accountsRouter);
+
+server.get('/', (req,res) => {
+    res.status(200).send('<h1>Get Ready to Rock and Roll</h2>');
+});
 
 module.exports = server;
